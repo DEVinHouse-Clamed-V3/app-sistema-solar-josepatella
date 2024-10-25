a16ff9f98d8b5fa5582b8d7aa5d654076fb04731
Patella <jfpatella@gmail.com>
2024-10-25 18:48:17 -0300

    Renderização do array de planetas na tela.

--- SistemaSolar/App.js
+++ SistemaSolar/App.js
@@ -1,18 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Image, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { planetas } from './planetas'
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-            <StatusBar backgroundColor={"#21005d"} />
+            <StatusBar backgroundColor={"#247BA0"} />
       <View style={styles.topo}>
       <Text>Vamos explorar!</Text>
       <Image style={styles.img} source={{
           uri: 'https://media.hswstatic.com/eyJidWNrZXQiOiJjb250ZW50Lmhzd3N0YXRpYy5jb20iLCJrZXkiOiJnaWZcL291dGVyLXNwYWNlLmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6ODI4fX19',
         }} />
       </View>
+<ScrollView>
+  {
+    planetas.map((planeta) => (
+<View style={styles.card}>
+<Text style={{alignSelf: 'center', fontWeight: 'bold'}}>{planeta.nome}</Text>
+<Image style={styles.imgPlaneta} source={{
+          uri: planeta.img
+        }} />
+        <Text>{`Average Orbital Speed: ` + planeta.velocidadeOrbitalMediaKmS}</Text>
+        <Text>{`Satellites: ` + planeta.quantidadeSatelites}</Text>
+        <Text>{`Surface Área: ` + planeta.areaSuperficieKm2}</Text>
+        <Text>{`Rotation Period: `+ planeta.periodoRotacaoDias}</Text>
+</View>
+    ))
 
-
+}
+</ScrollView>
     </SafeAreaView>
   );
 }
@@ -20,9 +36,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E8F1F2',
     alignItems: 'center',
-    margin: 50
+    padding: 45
   },
   img: {
     width: 30,
@@ -31,5 +47,12 @@ const styles = StyleSheet.create({
   topo: {
     flexDirection: 'row',
     gap: 10
+  },
+  card:{
+
+  },
+  imgPlaneta: {
+    width: 300,
+    height: 300,
   }
 });
